800e45088760b3298afd9c48eacb9e8e88ffeff9
Lupo Montero <lupomontero@gmail.com>
2018-10-16 23:50:26 -0500

    cleaning up...

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ Promise.all([
   import('image-to-ascii'),
   navigator.mediaDevices.getUserMedia({ video: true }),
 ])
-  .then(([imageToAscii, stream ]) => {
+  .then(([imageToAscii, stream]) => {
     const streamSettings = stream.getVideoTracks()[0].getSettings();
     const width = 80;
     const height = streamSettings.height * (width / streamSettings.width);
@@ -36,7 +36,6 @@ Promise.all([
     const update = () => {
       stats.begin();
       ctx.drawImage(video, 0, 0, width, height);
-      const { data } = ctx.getImageData(0, 0, width, height);
       pre.innerHTML = imageToAscii.fromCanvas(canvas, 'plain');
       stats.end();
       requestAnimationFrame(update);
